37ee80b3ed06137eba29507f5299c7c544b55ebd
Johan Quinde <125622945+Jhons23@users.noreply.github.com>
2023-03-31 18:53:12 -0500

    Add files via upload

---
+++ C++ Códigos/Programacion/calculadora.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y,s,r,d,m;
+	cout <<"Ingrese el primer valor"; cin >>x;
+	cout <<"Ingrese el segundo valor"; cin >>y;
+	s=x+y;
+	r=x-y;
+	d=x/y;
+	m=x*y;
+	cout <<"El resultado de la suma es:"<<x<<"+"<<y<<"="<<s<<endl;
+	cout <<"El resultado de la resta es:"<<x<<"-"<<y<<"="<<r<<endl;
+	cout <<"El resultado de la division es"<<x<<"/"<<y<<"="<<endl;
+	cout <<"El resultado de la multiplicacion es";<<x<<"*"<<y<<"="<<endl;
+
+
+
+
+
+
+
+return 0;
+
+}
