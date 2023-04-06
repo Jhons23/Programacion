72e3f2dcf505606a44495fc0ebc6012157d61b2f
Johan Quinde <125622945+Jhons23@users.noreply.github.com>
2023-04-06 18:11:57 -0500

    Update Suma de numeros consecutivos.cpp

--- C++ Códigos/Suma de numeros consecutivos.cpp
+++ C++ Códigos/Suma de numeros consecutivos.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
+
 int main ()
 {
     int n, suma=0;
     cout <<"Ingrese el numero";
     cin >>n;
-    for (i=1; i<=n; i++)
+    for (int i=1; i<=n; i++)
     suma += i;
     {
         cout <<"La suma es:"<<suma<<endl;
